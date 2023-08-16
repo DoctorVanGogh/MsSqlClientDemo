8aeb4caa9bb4d6432082a4f2c20de607806e60e7
DoctorVanGogh <DoctorVanGogh@users.noreply.github.com>
2023-08-16 20:11:10 +0200

    system sqlclient "base" implementation

---
+++ ConsoleApp8/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+using ConsoleApp8.Model;
+
+namespace ConsoleApp8 {
+    internal class Program {
+        static void Main(string[] args) {
+
+            try {
+                using (var ctx = new AdventureWorksEntities()) {
+                    Console.WriteLine($"{ctx.Address.Count()} adresses");
+                }
+            } catch (Exception ex) {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+    }
+}
